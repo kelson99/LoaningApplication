bd74b68770a2d793ca634c26064e2682014517ae
Kelson <kelson99hartle@icloud.com>
2022-07-20 21:44:54 -0500

    Added Xcode Project

---
+++ LoaningApplication/LoaningApplication/LoaningApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoaningApplicationApp.swift
+//  LoaningApplication
+//
+//  Created by Kelson Hartle on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LoaningApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
